8827c274ecc0a75fd307b82bb716d9fc26b3f1c3
Dmitry1512 <dima.kirshchin1512@gmail.com>
2024-01-31 22:12:04 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//мобильнок бургер меню
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.header__nav-link');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('open');
+});
+navLinks.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    burger.classList.toggle('active');
+    nav.classList.toggle('open');
+  });
+});
+
+//Плавный скролл к якорю
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+
+
